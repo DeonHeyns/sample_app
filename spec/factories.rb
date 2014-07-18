52f6d722c88539dc8b8149534d1e8d41ae9dea2d
Deon Heyns <heynsd@gmail.com>
2014-07-17 22:36:58 -0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name                  "Deon Heyns"
+		email                 "deon@deonheyns.com"
+		password              "Joshua!2"
+		password_confirmation "Joshua!2"
+	end
+end
