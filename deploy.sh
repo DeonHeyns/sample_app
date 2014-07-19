5aa07f13b220ca03b058bf3acc2c745bcca9e61e
Deon Heyns <DeonHeyns@Deons-MacBook-Pro.local>
2014-07-19 11:23:07 -0300

    Updated formatting

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 read -e -p "Enter your commit message: " commit_message
 env -i git add --all
-env -i git commit -m '$commit_message'
-env -i git push origin master
-env -i git push heroku master
+env -i git commit -m "$commit_message"
+env -i git push -u origin master
+env -i git push -u heroku master
 heroku run rake db:migrate
 heroku open
