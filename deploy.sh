1beebabea2ff6151a01a275d4f3af94a4bff92a4
Deon Heyns <DeonHeyns@Deons-MacBook-Pro.local>
2014-07-22 19:24:33 -0300

    Finish user edit, update, index and destory actions

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 read -e -p "Enter your commit message: " commit_message
 env -i git add --all
 env -i git commit -m "$commit_message"
+env -i git co master
+read -e -p "Branch to merge: " branch
+env -i git merge $branch
 env -i git push -u origin master
 env -i git push -u heroku master
 heroku run rake db:migrate
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name                  "Deon Heyns"
-		email                 "deon@deonheyns.com"
-		password              "Joshua!2"
-		password_confirmation "Joshua!2"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
